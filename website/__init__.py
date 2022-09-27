ff7b7eebed43e5e6779f71f66396453eef82af01
Tobias Hungwe <tobias.hungwe@student.howest.be>
2022-09-28 00:27:05 +0200

    Add autorization state

--- website/__init__.py
+++ website/__init__.py
@@ -1,3 +1,4 @@
+from re import L
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
@@ -17,9 +18,25 @@ def create_app():
 
     from .views import views
     from .auth import auth
-    
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id): 
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+     if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
