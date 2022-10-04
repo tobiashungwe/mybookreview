97ed0fc02562dfa74e8c7f49f8c1b205ceebb67d
Tobias Hungwe <tobias.hungwe@student.howest.be>
2022-10-04 04:48:06 +0200

    Update  dataframe with different parameters

--- Services/Books/Books_dataframe.py
+++ Services/Books/Books_dataframe.py
@@ -1,33 +1,44 @@
+import json
 import pandas
 
-from Models.Books import BookVolumes
+from Models.Books_volumes import BookVolumes
 
-from Services.Books_insert import save_book
+from Services.Books.Books_insert import save_book
 
 import requests
 
+from Utilities.Methods import error_response
 
 
-def df_pandas_book(book: str) -> str:
-    ress = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={book}")
 
-    if ress.status_code == 200:
+def df_pandas_book(books: str) -> str:
+    
 
-        df_items = pandas.DataFrame(ress.json()["items"])
+        ress = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={books}&maxResults=20")
 
-        df = pandas.DataFrame()
+        if ress.status_code == 200:
 
-        for x in range(df_items["volumeInfo"].index.size):
-            book = BookVolumes(ress.json()["title"],
-                                 ress.json()["imageLinks"]["smallThumbnail"],
-                                 ress.json()["subtitle"],
-                                 ress.json()["authors"],
+            df_items = pandas.DataFrame(ress.json()["items"])
+
+            df = pandas.DataFrame()
+            
+            
+            for x in range(df_items["volumeInfo"].index.size):
+                
+                ress.json()['items'][x]['volumeInfo'].get('description', 'No discription')
+                    
+                book = BookVolumes(ress.json()['items'][x]['volumeInfo']['title'],
+                                 ress.json()['items'][x]['volumeInfo']["imageLinks"]["thumbnail"],
+                                 ress.json()['items'][x]['volumeInfo'].get('description', 'No discription was given'),
+                                 ress.json()['items'][x]['volumeInfo'].get('authors', 'No authors was given')
                                  )
 
-            df = df.append(book.__dict__(), ignore_index=True)
-            
-            save_book(BookVolumes(book.name, book.pic, book.subtitle, book.authors)) # Insert Class in MongoDB.
+                print(book)
+                df = df.append(book.__dict__(), ignore_index=True)
+                print(df)    
+                save_book(BookVolumes(book.title, book.pic, book.description, book.authors)) # Insert Class in MongoDB.
 
-        return df.to_json(orient="records") # return a json classification for records
-    else:
-        return {"code": ress.status_code}
+            return df.to_json(orient="records") # return a json classification for records
+        else:
+            return {"code": ress.status_code}
+
